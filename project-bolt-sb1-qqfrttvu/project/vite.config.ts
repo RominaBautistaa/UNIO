918e2178c8263d5df3cbdcf11a3f3a8271c44171
Romina Bautista Arrilucea <rominabautistaa0@gmail.com>
2025-11-14 23:20:54 -0500

    ultimos cambios

--- project-bolt-sb1-qqfrttvu/project/vite.config.ts
+++ project-bolt-sb1-qqfrttvu/project/vite.config.ts
@@ -7,4 +7,23 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/servicio\/estrategia-creativa$/, to: '/servicio-estrategia.html' },
+        { from: /^\/servicio\/automatizacion-campanas$/, to: '/servicio-automatizacion.html' },
+        { from: /^\/servicio\/metricas-inteligentes$/, to: '/servicio-metricas.html' },
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        estrategia: './servicio-estrategia.html',
+        automatizacion: './servicio-automatizacion.html',
+        metricas: './servicio-metricas.html',
+      },
+    },
+  },
 });
